929b58c8f3debf248930fe85b676781277b96f36
peterzeng <peterzengcc@gmail.com>
2018-11-01 19:45:27 +0800

    finish exr format

--- data/WorldExpo/perspective_blur.py
+++ data/WorldExpo/perspective_blur.py
@@ -7,7 +7,8 @@ from matplotlib import pyplot as plt
 from scipy.ndimage.filters import gaussian_filter
 from matplotlib import pyplot as plt
 from exr_utils import load_exr, save_exr
-
+import os
+from pathlib import Path
 
 def generate_dmap(head_locs, pmap):
     dmap = np.zeros(pmap.shape[0:2], dtype='float32')
@@ -27,42 +28,56 @@ def generate_dmap(head_locs, pmap):
 
 
 def main():
-    # fname_image = "100156_A02IndiaWE-03-S20100626080000000E20100626233000000_new.split.5_1.jpg"
-    # fname_label = "100156_A02IndiaWE-03-S20100626080000000E20100626233000000_new.split.5_1.mat"
-    fname_image = "100156_A02IndiaWE-03-S20100626080000000E20100626233000000_new.split.75_3.jpg"
-    fname_label = "100156_A02IndiaWE-03-S20100626080000000E20100626233000000_new.split.75_3.mat"
-    fname_pmap = "100156.mat"
-    file_pmap = sio.loadmat(fname_pmap)
-    file_head_labels = sio.loadmat(fname_label)
-    img = io.imread(fname_image)
-    pmap = file_pmap["pMap"]
-    head_locs = file_head_labels["point_position"]
-    dmap = generate_dmap(head_locs, pmap)
-    # head_locs = file_head_labels["point_position"]
-    # num_heads = head_locs.shape[0]
-    # assert num_heads == file_head_labels["point_num"][0,0]
-    #
-    # labeled_image = img.copy()
-    # dmap = np.zeros(img.shape[0:2], dtype='float32')
-    # sigmas = []
-    # for i in range(num_heads):
-    #     x, y = head_locs[i]
-    #     labeled_image[y, x+range(-3,4),:] =255
-    #     single_density = np.zeros(dmap.shape, dtype='float32')
-    #     single_density[y, x] = 1.
-    #     sigma = 0.2*pmap[y,x]
-    #     sigmas.append(sigma)
-    #     dmap += gaussian_filter(single_density, sigma)
-    # dmap_sum = np.sum(dmap)
-    # norm_factor = num_heads/dmap_sum
-    # dmap = norm_factor * dmap
+    data_dir = "/mnt/m2/mzcc/crowd_data/worldexpo"
+    prefix = "train"
+    label_top_dir = "{}/{}_label".format(data_dir, prefix)
+    persp_dir = "{}/{}_perspective".format(data_dir, prefix)
+    output_dir = "{}/{}_dmap".format(data_dir, prefix)
+    if not os.path.exists(output_dir):
+        print("creating dir: {}".format(output_dir))
+        os.mkdir(output_dir)
+    for label_subd in os.listdir(os.fsencode(label_top_dir)):
+        name_label_subd = os.fsdecode(label_subd)
+        persp_fname = "{}/{}.mat".format(persp_dir, name_label_subd)
+        assert os.path.exists(persp_fname)
+        file_pmap = sio.loadmat(persp_fname)
+        pmap = file_pmap["pMap"]
+        print(os.fsdecode(label_subd))
+        this_scene_path = "{}/{}".format(label_top_dir,name_label_subd)
+        for label_name in os.listdir(os.fsencode(this_scene_path)):
+            name_label = os.fsdecode(label_name)
+            if name_label == "roi.mat":
+                continue
+            path_label = "{}/{}".format(this_scene_path,name_label)
+            # print(path_label)
+            file_head_labels = sio.loadmat(path_label)
+            head_locs = file_head_labels["point_position"]
+            dmap = generate_dmap(head_locs, pmap)
+            output_path = "{}/{}.exr".format(output_dir,name_label[:-4])
+            print(output_path)
+            save_exr(output_path, dmap)
 
-    plt.figure(1)
-    plt.imshow(img)
-    plt.figure(2)
-    plt.imshow(dmap)
-    save_exr("{}.exr".format(fname_image),dmap)
-    plt.show()
+
+
+    # fname_image = "100156_A02IndiaWE-03-S20100626080000000E20100626233000000_new.split.75_3.jpg"
+    # fname_label = "100156_A02IndiaWE-03-S20100626080000000E20100626233000000_new.split.75_3.mat"
+    # fname_pmap = "100156.mat"
+    # file_pmap = sio.loadmat(fname_pmap)
+    # file_head_labels = sio.loadmat(fname_label)
+    # img = io.imread(fname_image)
+    # pmap = file_pmap["pMap"]
+    # head_locs = file_head_labels["point_position"]
+    # dmap = generate_dmap(head_locs, pmap)
+    #
+    #
+    # plt.figure(1)
+    # plt.imshow(img)
+    # plt.figure(2)
+    # plt.imshow(dmap)
+    # plt.show()
+    #
+    # save_exr("{}.exr".format(fname_image),dmap)
+
     return
 
 if __name__ == "__main__":
--- data/WorldExpo/video2image.sh
+++ data/WorldExpo/video2image.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
+cd ./train_video
+TARGET_DIR=train_full_frames
+mkdir ../${TARGET_DIR}
+for video_file_name in *.avi; do
+    echo ${video_file_name}
+    mkdir ../${TARGET_DIR}/${video_file_name}
+    ffmpeg -i ${video_file_name} -f image2 ../${TARGET_DIR}/${video_file_name}/f%06d.jpg
+done
+
 cd ./test_video
 TARGET_DIR=test_full_frames
 mkdir ../${TARGET_DIR}
